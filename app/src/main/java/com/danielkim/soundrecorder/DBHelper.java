dab15b6a0ef50f9e722db6a025160aef9d4c60e1
Max Zorzetti <maxzorzetti@Maxs-MacBook-Pro.local>
2017-11-07 20:34:21 -0200

    Returns rowId variable without storing it first on DBHelper

--- app/src/main/java/com/danielkim/soundrecorder/DBHelper.java
+++ app/src/main/java/com/danielkim/soundrecorder/DBHelper.java
@@ -155,8 +155,7 @@ public class DBHelper extends SQLiteOpenHelper {
         cv.put(DBHelperItem.COLUMN_NAME_RECORDING_LENGTH, item.getLength());
         cv.put(DBHelperItem.COLUMN_NAME_TIME_ADDED, item.getTime());
         cv.put(DBHelperItem._ID, item.getId());
-        long rowId = db.insert(DBHelperItem.TABLE_NAME, null, cv);
-
-        return rowId;
+ 
+        return db.insert(DBHelperItem.TABLE_NAME, null, cv);
     }
 }
